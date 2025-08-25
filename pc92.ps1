0365e776c48fe9636ab59a83dca78fe942eb319f
aldrin131989 <alozih131989@gmail.com>
2025-08-25 15:40:22 +0800

    Add files via upload

---
+++ pc92.ps1
@@ -0,0 +1,12 @@
+﻿Implementition Data Confidentiality
+using Public Keys:
+@coreBABA:
+config t
+username admin privilege 15 secret pass
+username aldrin privilege 15 secret pass
+ip domain-name pfda.gov.ph
+crypto key generated rsa
+!! tanong, anong KeySize: 256, 512, 1024
+ip ssh version 2
+ip ssh time-out 120
+end
